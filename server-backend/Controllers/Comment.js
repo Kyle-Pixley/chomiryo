a2a2ba608f3de9003580883a9b31aeb498e30f42
KylePixley <kyle_pixley@hotmail.com>
2024-11-01 17:03:56 -0400

    get all comments for post fix

--- server-backend/Controllers/Comment.js
+++ server-backend/Controllers/Comment.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Comment = require("../Models/Comment");
 const sessionValidation = require("../Middlewares/Session");
 
+//create a comment for a specific post
 router.post("/create/:postId", sessionValidation, async (req, res) => {
     try {
 
@@ -39,11 +40,11 @@ router.get('/:postId', sessionValidation, async (req, res) => {
     try {
         const { postId } = req.params;
 
-        const comments = await Comment.find({ postReceiver: postId }).populate("sender", "username");
+        const comments = await Comment.find({ postReceiver: postId }).populate("user","body");
 
         res.status(200).json({
             comments,
-            message: 'comments recieved'
+            message: 'comments received'
         });
     } catch(err) {
         console.log(err);
