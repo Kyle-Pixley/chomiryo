415942d9ff03edfb1b115839adb131a07f99fba8
KylePixley <kyle_pixley@hotmail.com>
2024-11-02 15:26:46 -0400

    front page styling/wip

--- chomiryo/src/Components/FrontPage/FrontPage.jsx
+++ chomiryo/src/Components/FrontPage/FrontPage.jsx
@@ -5,12 +5,19 @@ import './FrontPage.css';
 function FrontPage({ logout }) {
   return (
     <div id='front-page-component'>
-        <div id='top-banner'>might change this depending on the weight of the pictures users submit</div>
-        <div id='top-banner-bottom-border'></div>
+        <div id='top-banner'>might change this depending on the "weight" of the pictures users submit</div>
+        <div id='top-banner-bottom-border'>
+            <p 
+                id='create-recipe-button'
+                onClick={console.log('create-post/recipe')} >Post Recipe</p>
+            <p 
+                id='logout-button'
+                onClick={logout}>Logout</p>
+        </div>
         FrontPage
         
     </div>
   )
 }
 
-export default FrontPage
+export default FrontPage;
